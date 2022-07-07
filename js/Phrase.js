944c969ba7be69a335967459b3be73c9bd07714f
Stelko <stilyanmtoskov@gmail.com>
2022-07-07 20:29:20 +0100

    phrase constructor + addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,24 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        let phraseSection = document.getElementById("phrase");
+        let phraseHTML = "<ul>";
+
+        for (let i of this.phrase) {
+            if (i === " ") {
+                phraseHTML += `
+    <li class="space"> </li>`;
+            } else {
+                phraseHTML += `
+    <li class="hide letter ${i}">${i}</li>`;
+            }
+        }
+        
+        phraseHTML += `
+</ul>`;
+        phraseSection.innerHTML = phraseHTML;
+    }
+}
