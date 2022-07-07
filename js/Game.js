f66650b010ab24e2865e26ef0b2c66f7676f297b
Stelko <stilyanmtoskov@gmail.com>
2022-07-07 20:52:32 +0100

    game constructors

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase("Down To Earth"),
+            new Phrase("Shot In The Dark"),
+            new Phrase("Go For Broke"),
+            new Phrase("A Piece of Cake"),
+            new Phrase("Jumping The Gun")
+        ];
+        this.activePhrase = null;
+    }
+}
