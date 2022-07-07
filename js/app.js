393b2cf1b75e9507456a57667765778ea0c6e013
Stelko <stilyanmtoskov@gmail.com>
2022-07-07 22:43:35 +0100

    start game btn, keyboardbtns listener

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@
 let phraseSection = document.getElementById("phrase");
 let letters = document.getElementsByClassName("letter");
 let startOverlay = document.getElementById("overlay");
+let startGameBtn = document.getElementById("btn__reset");
+let keyboardBtns = document.getElementById("qwerty");
+const game = new Game();
+
+startGameBtn.addEventListener("click", function() {game.startGame()});
+
+keyboardBtns.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        game.handleInteraction(e.target)
+    };
+})
