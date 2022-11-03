833482c1da1d7f7550213f2b480c22cf3fa7f2cb
stelko <stilyanmtoskov@gmail.com>
2022-11-03 16:46:44 +0000

    Game Class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            "Down To Earth",
+            "Shot In The Dark",
+            "Go For Broke",
+            "A Piece of Cake",
+            "Jumping The Gun",
+        ];
+        this.activePhrase = null;
+    }
+}
