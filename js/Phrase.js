cad2de3523c1c580d3db7cf94610841ec12030b9
stelko <stilyanmtoskov@gmail.com>
2022-11-03 16:20:54 +0000

    addPhraseToDisplay added to Phrase

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,4 +2,14 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+    addPhraseToDisplay() {
+        let phraseHTML = ``;
+        for (let i = 0; i < this.phrase.length; i++) {
+            if (this.phrase[i] === ' ') {
+                phraseHTML += `<li class="space"> </li>`;
+            } else {
+                phraseHTML += `<li class="hide letter ${this.phrase[i]}">${this.phrase[i]}</li>`;
+            }
+        }
+    }
 }
